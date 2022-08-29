0fbd52c52ead4f21b54bd96ed99617f2d884df0f
TopWork0ut <olegiksen@gmail.com>
2022-08-29 12:26:17 +0300

    added second lab

---
+++ Lab2/js/main.js
@@ -0,0 +1,12 @@
+// trial variant
+// const OPEN_CLASSNAME = "open";
+
+// const navLinks = document.getElementById("");
+
+// function toggleMenu() {
+//     if(navLinks.classList.contains(OPEN_CLASSNAME)){
+//         navLinks.classList.remove(OPEN_CLASSNAME);
+//     } else {
+//         navLinks.classList.add(OPEN_CLASSNAME);
+//     }
+// }
